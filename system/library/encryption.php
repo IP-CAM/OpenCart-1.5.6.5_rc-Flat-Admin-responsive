a24bd1655cee247973cefdfbacf33e009a454c28
Ernie <ernst@jacob.ch>
2020-10-22 01:24:13 +0200

    Update encryption.php
    Make this Version work with PVP v.7.x Versions

--- system/library/encryption.php
+++ system/library/encryption.php
@@ -1,19 +1,28 @@
 <?php
 final class Encryption {
+	
+	private $cipher = 'aes-256-ctr';
+	private $digest = 'sha256';
 	private $key;
-	private $iv;
 	
 	public function __construct($key) {
-		$this->key = hash('sha256', $key, true);
-		$this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
+		$this->key = $key;
 	}
-	
+
 	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $value, MCRYPT_MODE_ECB, $this->iv)), '+/=', '-_,');
+		$key       = openssl_digest($this->key, $this->digest, true);
+		$iv_length = openssl_cipher_iv_length($this->cipher);
+		$iv        = openssl_random_pseudo_bytes($iv_length);
+		return base64_encode($iv . openssl_encrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv));
 	}
 	
 	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB, $this->iv));
+		$key       = openssl_digest($this->key, $this->digest, true);
+		$iv_length = openssl_cipher_iv_length($this->cipher);
+		$value     = base64_decode($value);
+		$iv        = substr($value, 0, $iv_length);
+		$value     = substr($value, $iv_length);
+		return openssl_decrypt($value, $this->cipher, $key, OPENSSL_RAW_DATA, $iv);
 	}
 }
 ?>
